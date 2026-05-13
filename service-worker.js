b9276ecd3b711834f4895c8e399ad1196de8bf11
Elevari <36955032+Elevari@users.noreply.github.com>
2026-05-13 11:05:09 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@
    ClassTrack Service Worker  v1.0
    ============================================================ */
 
-const CACHE_NAME      = 'classtrack-v1';
-const DATA_CACHE_NAME = 'classtrack-data-v1';
+const CACHE_NAME      = 'classtrack-v2';
+const DATA_CACHE_NAME = 'classtrack-data-v2';
 
 const STATIC_ASSETS = [
   './',
